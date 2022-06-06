07b82d426c40e0debc2c52d1e54433b867c800f9
lnuvy <hangeulgod@naver.com>
2022-06-06 19:23:08 +0900

    first commit

---
+++ webpack.config.ts
@@ -0,0 +1,22 @@
+// import path from 'path'
+// // import ReactRefreshWebpackPlugin from
+//
+//
+// const isDevelopment = process.env.NODE_ENV !== 'production'
+//
+// const config: webpack.Configuration = {
+//     name: 'sleact',
+//     mode: isDevelopment ? 'development' : 'production',
+//     devtool: isDevelopment ? 'hidden-source-map' : 'eval',
+//     resolve: {
+//         extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+//         alias: {
+//             '@hooks': path.resolve(__dirname, 'hooks'),
+//             '@components': path.resolve(__dirname, 'components'),
+//             '@layouts': path.resolve(__dirname, 'layouts'),
+//             '@pages': path.resolve(__dirname, 'pages'),
+//             '@utils': path.resolve(__dirname, 'utils'),
+//             '@typings': path.resolve(__dirname, 'typings'),
+//         }
+//     }
+// }
