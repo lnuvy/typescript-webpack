acf906fda1dff190a6b624b9428573ddf1428c1e
lnuvy <hangeulgod@naver.com>
2022-06-06 19:59:17 +0900

    TS_NODE_PROJECT가 인식되지 않음

---
+++ layouts/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = ()=> {
+    return <div> 좀만 참아</div>
+}
+
+export default App;
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,8 +1,8 @@
 import path from 'path'
-import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+// import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import webpack from 'webpack'
-import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
-import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
+// import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
+// import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
@@ -59,28 +59,40 @@ const config: webpack.Configuration = {
         ]
     },
     plugins: [
-        new ForkTsCheckerWebpackPlugin({
-            async: false,
-            // eslint: {
-            //   files: "./src/**/*",
-            // },
-        }),
-        new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' }),
+        // new ForkTsCheckerWebpackPlugin({
+        //     async: false,
+        //     // eslint: {
+        //     //   files: "./src/**/*",
+        //     // },
+        // }),
+        new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' }), // NODE_ENV 를 프론트에서 사용할 수 있게 설정
     ],
     output: {
         path: path.join(__dirname, 'dist'),
         filename: '[name].js',
         publicPath: '/dist/',
     },
-    devServer: {
-        historyApiFallback: true, // react router
-        port: 3090,
-        publicPath: '/dist/',
-        proxy: {
-            '/api/': {
-                target: 'http://localhost:3095',
-                changeOrigin: true,
-            },
-        },
-    },
+    // devServer: {
+    //     historyApiFallback: true, // react router
+    //     port: 3090,
+    //     publicPath: '/dist/',
+    //     proxy: {
+    //         '/api/': {
+    //             target: 'http://localhost:3095',
+    //             changeOrigin: true,
+    //         },
+    //     },
+    // },
 }
+
+if (isDevelopment && config.plugins) {
+    // config.plugins.push(new webpack.HotModuleReplacementPlugin());
+    // config.plugins.push(new ReactRefreshWebpackPlugin());
+    // config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'server', openAnalyzer: true }));
+}
+if (!isDevelopment && config.plugins) {
+    // config.plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }));
+    // config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'static' }));
+}
+
+export default config
