0e4275585ade0425ba175d931c518d47d48807c6
lnuvy <lnuvy.code@gmail.com>
2022-06-07 21:26:58 +0900

    webpack-dev-server@4 버전에 맞게 수정

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,8 +1,14 @@
 import path from 'path'
-// import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
-import webpack from 'webpack'
-// import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
+import webpack, { Configuration as WebpackConfiguration } from "webpack";
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
 // import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
+import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server";
+
+
+interface Configuration extends WebpackConfiguration {
+    devServer?: WebpackDevServerConfiguration;
+}
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
@@ -59,12 +65,12 @@ const config: webpack.Configuration = {
         ]
     },
     plugins: [
-        // new ForkTsCheckerWebpackPlugin({
-        //     async: false,
-        //     // eslint: {
-        //     //   files: "./src/**/*",
-        //     // },
-        // }),
+        new ForkTsCheckerWebpackPlugin({
+            async: false,
+            // eslint: {
+            //   files: "./src/**/*",
+            // },
+        }),
         new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' }), // NODE_ENV 를 프론트에서 사용할 수 있게 설정
     ],
     output: {
@@ -72,22 +78,17 @@ const config: webpack.Configuration = {
         filename: '[name].js',
         publicPath: '/dist/',
     },
-    // devServer: {
-    //     historyApiFallback: true, // react router
-    //     port: 3090,
-    //     publicPath: '/dist/',
-    //     proxy: {
-    //         '/api/': {
-    //             target: 'http://localhost:3095',
-    //             changeOrigin: true,
-    //         },
-    //     },
-    // },
+    devServer: {
+        historyApiFallback: true,
+        port: 3090,
+        devMiddleware: { publicPath: '/dist/' },
+        static: { directory: path.resolve(__dirname) },
+    },
 }
 
 if (isDevelopment && config.plugins) {
-    // config.plugins.push(new webpack.HotModuleReplacementPlugin());
-    // config.plugins.push(new ReactRefreshWebpackPlugin());
+    config.plugins.push(new webpack.HotModuleReplacementPlugin());
+    config.plugins.push(new ReactRefreshWebpackPlugin());
     // config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'server', openAnalyzer: true }));
 }
 if (!isDevelopment && config.plugins) {
