e3faeefdd7cf7fb9271344326aaccd018153c72d
lnuvy <lnuvy.code@gmail.com>
2022-06-09 22:54:07 +0900

    영상 재생이 또 왜안되니ㅠ

--- client.tsx
+++ client.tsx
@@ -1,6 +1,11 @@
 import React from "react"
 import {render} from 'react-dom'
 
-import App from './layouts/App'
+import App from '@layouts/App'
 
 render(<App />, document.querySelector('#app'))
+
+
+// pages - 서비스 페이지
+// components -
+// layouts -  공통적인 레이아웃
