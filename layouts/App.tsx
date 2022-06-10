ad02673153a41df33a6265c0fbadebecaaf979ba
lnuvy <lnuvy.code@gmail.com>
2022-06-10 21:12:43 +0900

    6버전 타입스크립트 적용되는지 확인해보기

--- layouts/App.tsx
+++ layouts/App.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Routes, Redirect } from 'react-router-dom'
+import Login from '@pages/login'
+import SignUp from '@pages/signUp'
 
 const App = ()=> {
     return <div> 핫 리로딩~</div>
---
+++ pages/signUp/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div>
+      핫 리로딩
+    </div>
+  )
+}
+
+export default SignUp
