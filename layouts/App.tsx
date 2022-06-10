ae3e37dec694779fe8850d6ba378de8f1240f52c
lnuvy <lnuvy.code@gmail.com>
2022-06-10 21:15:34 +0900

    6버전 타입스크립트 적용되는지 확인해보기

--- layouts/App.tsx
+++ layouts/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Routes, Redirect } from 'react-router-dom'
+import { Routes } from 'react-router-dom'
 import Login from '@pages/login'
 import SignUp from '@pages/signUp'
 
 const App = ()=> {
-    return <div> 핫 리로딩~</div>
+    return <div> react-router-dom v6 에선 Redirect가 없고 replace props 로 대신한다</div>
 }
 
 export default App;
