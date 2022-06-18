a02fbe68e9198eb932f214a81f3e61da4b69178c
lnuvy <lnuvy.code@gmail.com>
2022-06-18 18:53:15 +0900

    6버전아 덤벼라

--- layouts/App.tsx
+++ layouts/App.tsx
@@ -4,7 +4,7 @@ import Login from '@pages/login'
 import SignUp from '@pages/signUp'
 
 const App = ()=> {
-    return <div> react-router-dom v6 에선 Redirect가 없고 replace props 로 대신한다</div>
+    return <div> 6버전 덤벼</div>
 }
 
 export default App;
