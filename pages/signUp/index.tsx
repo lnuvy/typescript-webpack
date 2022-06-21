80c5708a097d4988ae57932c2d7f11afdb78b982
lnuvy <hangeulgod@naver.com>
2022-06-22 08:10:25 +0900

    onChange의 e와 onSubmit의 e 타입은 서로 다르다

--- pages/signUp/index.tsx
+++ pages/signUp/index.tsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
+import { Button, Form, Header, Input, Label, LinkContainer, Error } from '@pages/signUp/styles'
 
 const SignUp = () => {
+
+  const [email, setEmail] = useState('')
+  const [nickname, setNickname] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordCheck, setPasswordCheck] = useState('')
+
+  const [mismatchError, setMismatchError] = useState(false)
+
+  const onChangeEmail = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value)
+  }, [])
+
+  const onChangeNickname = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+      setNickname(e.target.value)
+    },[])
+
+  const onChangePassword = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value)
+    setMismatchError(e.target.value !== passwordCheck)
+  }, [passwordCheck])
+
+  const onChangePasswordCheck = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setPasswordCheck(e.target.value)
+    setMismatchError(e.target.value !== password)
+  }, [password])
+
+  const onSubmit = useCallback(
+    (e: React.FormEvent) => {
+      e.preventDefault()
+      if(!mismatchError) {
+        console.log('성공')
+      }
+    },
+    [email, nickname, password, passwordCheck, mismatchError],
+  );
+
+
+
   return (
-    <div>
-      핫 리로딩
+    <div id="container">
+      <Header>Sleact</Header>
+      <Form onSubmit={onSubmit}>
+        <Label id="email-label">
+          <span>이메일 주소</span>
+          <div>
+            <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail} />
+          </div>
+        </Label>
+        <Label id="nickname-label">
+          <span>닉네임</span>
+          <div>
+            <Input type="text" id="nickname" name="nickname" value={nickname} onChange={onChangeNickname} />
+          </div>
+        </Label>
+        <Label id="password-label">
+          <span>비밀번호</span>
+          <div>
+            <Input type="password" id="password" name="password" value={password} onChange={onChangePassword} />
+          </div>
+        </Label>
+        <Label id="password-check-label">
+          <span>비밀번호 확인</span>
+          <div>
+            <Input
+              type="password"
+              id="password-check"
+              name="password-check"
+              value={passwordCheck}
+              onChange={onChangePasswordCheck}
+            />
+          </div>
+          {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
+        </Label>
+        <Button type="submit">회원가입</Button>
+      </Form>
+      <LinkContainer>
+        이미 회원이신가요?&nbsp;
+        {/*<Link to="/login">로그인 하러가기</Link>*/}
+      </LinkContainer>
     </div>
   )
 }
