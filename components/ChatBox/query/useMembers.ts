8bfe6fbb99a75fbbf2acd111a432416d663f0ce3
lnuvy <lnuvy.code@gmail.com>
2022-07-30 18:30:13 +0900

    fetcher 적용

--- components/ChatBox/query/useMembers.ts
+++ components/ChatBox/query/useMembers.ts
@@ -1,5 +1,8 @@
 import { useQuery } from 'react-query';
+import { fetcher } from '@queries/fetcher';
 
 export const userMembers = (workspace: string) => {
   return useQuery(`/api/workspaces/${workspace}/members`, )
 }
+
+export const useGetUser = () => useQuery([`/api/member`], fetcher)
