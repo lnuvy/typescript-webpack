b7617f51d78fc03e3d496be6a622e0471445d65a
lnuvy <92131474+lnuvy@users.noreply.github.com>
2022-08-30 17:00:42 +0900

    Update index.tsx

--- components/ChatList/index.tsx
+++ components/ChatList/index.tsx
@@ -1,38 +1,38 @@
-// import React, { FC, forwardRef, useCallback, useRef } from 'react';
-// import { ChatZone, Section, StickyHeader } from '@components/ChatList/styles';
-// import { IDM } from '@typings/db';
-// import Chat from '@components/Chat';
-// import { Scrollbars } from 'react-custom-scrollbars-2';
-//
-// interface Props {
-//   chatSections: { [key: string]: IDM[] };
-// }
-//
-// const ChatList = forwardRef<Scrollbars, Props>(({ chatSections }, ref) => {
-//   const onScroll = useCallback((values: any) => {
-//     if (values.scrollTop === 0) {
-//       console.log('가장위');
-//     }
-//   }, []);
-//
-//   return (
-//     <ChatZone>
-//       <Scrollbars autoHide ref={ref} onScrollFrame={onScroll}>
-//         {Object.entries(chatSections).map(([date, chats]) => {
-//           return (
-//             <Section className={`section-${date}`} key={date}>
-//               <StickyHeader>
-//                 <button>{date}</button>
-//               </StickyHeader>
-//               {chats.map((chat) => {
-//                 return <Chat key={chat.id} data={chat} />;
-//               })}
-//             </Section>
-//           );
-//         })}
-//       </Scrollbars>
-//     </ChatZone>
-//   );
-// });
-//
-// export default ChatList;
+import React, { FC, forwardRef, useCallback, useRef } from 'react';
+import { ChatZone, Section, StickyHeader } from '@components/ChatList/styles';
+import { IDM } from '@typings/db';
+import Chat from '@components/Chat';
+import { Scrollbars } from 'react-custom-scrollbars-2';
+
+interface Props {
+  chatSections: { [key: string]: IDM[] };
+}
+
+const ChatList = forwardRef<Scrollbars, Props>(({ chatSections }, ref) => {
+  const onScroll = useCallback((values: any) => {
+    if (values.scrollTop === 0) {
+      console.log('가장위');
+    }
+  }, []);
+
+  return (
+    <ChatZone>
+      <Scrollbars autoHide ref={ref} onScrollFrame={onScroll}>
+        {Object.entries(chatSections).map(([date, chats]) => {
+          return (
+            <Section className={`section-${date}`} key={date}>
+              <StickyHeader>
+                <button>{date}</button>
+              </StickyHeader>
+              {chats.map((chat) => {
+                return <Chat key={chat.id} data={chat} />;
+              })}
+            </Section>
+          );
+        })}
+      </Scrollbars>
+    </ChatZone>
+  );
+});
+
+export default ChatList;
